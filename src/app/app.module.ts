77f0eced288e85627de63024647cdafc2f2e1183
DoMinhHiep97 <54266147+DoMinhHiep97@users.noreply.github.com>
2019-11-12 14:21:17 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardNameComponent } from './card-name/card-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardNameComponent
   ],
   imports: [
     BrowserModule
